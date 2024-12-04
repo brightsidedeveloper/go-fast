57cabff14aef29fda078a6addda648af81183815
Josh Kennedy <976307+YoCodingJosh@users.noreply.github.com>
2024-12-04 12:58:51 -0600

    Docker and Database!

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
@@ -19,10 +23,20 @@ func main() {
 		MaxAge:           300,
 	}))
 
+	// TODO: put this in environment variables!!!!
+	dsn := "host=localhost user=postgres password=postgres dbname=postgres port=5432 sslmode=disable TimeZone=America/Chicago"
+	db, db_err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if db_err != nil {
+		log.Fatal(db_err)
+	}
+
+	println(fmt.Sprintf("Database connected: %v", db))
+
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Welcome to the API"))
 	})
-
+	println("Go-Fast is running on port 8888")
 	err := http.ListenAndServe(":8888", router)
 	if err != nil {
 		log.Fatal(err)
